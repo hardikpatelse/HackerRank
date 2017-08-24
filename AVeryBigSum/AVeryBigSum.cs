ec1bc638941b0cb5f38717108b8b8a2f04e6765e
Hardik Patel <hardikpatelse@users.noreply.github.com>
2017-08-24 17:46:57 +0530

    AVeryBigSum in C#
    solution in c#

---
+++ AVeryBigSum/AVeryBigSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    class AVeryBigSum
+    {
+        static long aVeryBigSum(int n, long[] ar)
+        {
+            // Complete this function
+            long TotalResult = 0;
+            for (int i = 0; i < n; i++)
+            {
+                TotalResult += ar[i];
+            }
+            return TotalResult;
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            long[] ar = Array.ConvertAll(ar_temp, Int64.Parse);
+            long result = aVeryBigSum(n, ar);
+            Console.WriteLine(result);
+        }
+    }
+}
